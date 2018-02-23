7c582987a12e39655d1b2898d7ae54c4f9a01f44
Hackbright Student <no-reply@hackbrightacademy.com>
2018-02-22 20:31:12 -0800

    asks choice unless decide to exit

--- brighticorn_library.py
+++ brighticorn_library.py
@@ -8,27 +8,45 @@ def menu():
     print "Brighticorn's Books"
     print "-------------------"
     print "You can add/view/check/exit"
-    command = raw_input("Please enter your choice:  ")
-    print "Action: " + command
+    #command = raw_input("Please enter your choice: ")
+    #print "Action: " + command
 
-    command = command.lower()
-    if command == "exit":
-        print "goodbye!"
+    #command = command.lower()
+    while True:
+        command = raw_input("Please enter your choice: ")
+        command = command.lower()
+        print "Action: " + command
 
-    elif command == "add":
-        print "Add Book!"
-        new_book = raw_input("Which book would you like to add? ")
-        books.append(new_book)
-        print "Added: {}".format(new_book)
-        print books
+        if command == "exit":
+            print "goodbye!"
+            return
 
-    elif command == "view":
-        print "View Book!"
+        if command == "add":
+            print "Add Book!"
+            new_book = raw_input("Which book would you like to add? ")
+            books.append(new_book)
+            print "Added: {}".format(new_book)
+            print books
 
-    elif command == "check":
-        print "Check for a Book!"
+        elif command == "view":
+            print "View Book!"
+            for book in books:
+                print book
+            '''i = 0
+            while i < len(books):
+                print books[i]
+                i = i + 1'''
 
-    else:
-        print "Sorry, that is not a option of this program"
+        elif command == "check":
+            print "Check for a Book!"
+            to_check = raw_input("Which book would you like to check the library for? ")
+
+            if to_check in books:
+                print "Found {} in the library! ".format(to_check)
+            else:
+                print "{} not found. ".format(to_check)
+
+        else:
+            print "Sorry, that is not a option of this program"
 
 menu()
